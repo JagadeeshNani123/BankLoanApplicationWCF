70cefc9655cd4f7584f3e7f25a2624f2369ec4d5
cogdotnet1685 <cogdotnet1685@CTSDOTNET803>
2022-10-03 22:11:13 +0530

    Add project files.

---
+++ BankLoanApplicationWCF/WCFServices/LoanEMIService.svc.cs
@@ -0,0 +1,81 @@
+﻿using BankLoanApplicationWCF.DataContext;
+using BankLoanApplicationWCF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace BankLoanApplicationWCF.WCFServices
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class LoanEMIService
+    {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        LoanEMIDataContext ledc = new LoanEMIDataContext();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+      UriTemplate = "/AddLoanEMI",
+      RequestFormat = WebMessageFormat.Json,
+      ResponseFormat = WebMessageFormat.Json,
+      BodyStyle = WebMessageBodyStyle.Bare)]
+        public void AddLoanEMI(LoanEMIModel loanEMI)
+        {
+            ledc.InsertLoanEMI(loanEMI);
+        }
+
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+       UriTemplate = "/UpdateLoanEMI/{id}",
+       RequestFormat = WebMessageFormat.Json,
+       ResponseFormat = WebMessageFormat.Json,
+       BodyStyle = WebMessageBodyStyle.Bare)]
+        public void UpdateLoanEMI(LoanEMIModel loanEMI, string id)
+        {
+            var loanEMIId = new Guid(id);
+            ledc.UpdateLoanEMIUsingId(loanEMI, loanEMIId);
+        }
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+           RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           UriTemplate = "GetAllLoansEMIs")]
+        public List<LoanEMIModel> GetAllLoansEMIs()
+        {
+            return ledc.GetAllLoanEMIs();
+        }
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+        RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json,
+        UriTemplate = "GetLoanEMIById/{Id}")]
+        public LoanEMIModel GetLoanEMIById(string Id)
+        {
+            var loanEMIId = new Guid(Id);
+            return ledc.GetLoanEMIById(loanEMIId);
+        }
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+        RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json,
+        UriTemplate = "/DeleteLoanEMI/{id}")]
+        public void DeleteLoanEMI(string id)
+        {
+            var loanEMIId = new Guid(id);
+            ledc.DeleteLoanEMI(loanEMIId);
+        }
+    }
+}
---
+++ BankLoanApplicationWCF/WCFServices/LoanService.svc.cs
@@ -0,0 +1,79 @@
+﻿using BankLoanApplicationWCF.DataContext;
+using BankLoanApplicationWCF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace BankLoanApplicationWCF.WCFServices
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class LoanService
+    {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        LoanDataContext lc = new LoanDataContext();
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+       UriTemplate = "/AddLoan",
+       RequestFormat = WebMessageFormat.Json,
+       ResponseFormat = WebMessageFormat.Json,
+       BodyStyle = WebMessageBodyStyle.Bare)]
+        public void AddLoan(LoanModel loan)
+        {
+            lc.InsertLoan(loan);
+        }
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+         RequestFormat = WebMessageFormat.Json,
+         ResponseFormat = WebMessageFormat.Json,
+         UriTemplate = "/DeleteLoan/{id}")]
+        public void DeleteLoan(string id)
+        {
+            var loanId = new Guid(id);
+            lc.DeleteLoan(loanId);
+        }
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+           RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           UriTemplate = "GetAllLoans")]
+        public List<LoanModel> GetAllLoans()
+        {
+            return lc.GetAllLoans();
+        }
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+           RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           UriTemplate = "GetLoanById/{Id}")]
+        public LoanModel GetLoanById(string Id)
+        {
+            var loanId = new Guid(Id);
+            return lc.GetLoanById(loanId);
+        }
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+       UriTemplate = "/UpdateLoan/{id}",
+       RequestFormat = WebMessageFormat.Json,
+       ResponseFormat = WebMessageFormat.Json,
+       BodyStyle = WebMessageBodyStyle.Bare)]
+        public void UpdateLoan(LoanModel loan, string id)
+        {
+            var loanId = new Guid(id);
+            lc.UpdateLoanUsingId(loan, loanId);
+        }
+    }
+}
